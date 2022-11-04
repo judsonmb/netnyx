7e5f01da1fdf0b0f4044b70501977c5f43b3d560
Judson Bandeira <judson.bandeira@fichacerta.com.br>
2022-11-04 12:14:46 -0300

    login and register page, translate the framework pages to pt_BR and insert the role column on migration

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    if (Auth::user()) {
+        return redirect('home');
+    }
+    return view('auth.login');
 });
 
 Auth::routes();
