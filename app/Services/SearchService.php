33f00f2ea141b7c5087888afd67a1206287f7e47
Judson Bandeira <judson.bandeira@fichacerta.com.br>
2022-11-05 16:44:29 -0300

    por deixando aparecer o media type person pois retirando da listagem estava influenciando no numero dos botoes da paginacao. a busca deve ser filtrada de alguma forma para nao vir os person.

--- app/Services/SearchService.php
+++ app/Services/SearchService.php
@@ -5,16 +5,8 @@ use Http;
 
 class SearchService
 {
-    public function getToken()
-    {
-        $url = config('constants.apiUrl') . '/authentication/token/new?api_key='.config('constants.apiKey');
-        $response = Http::get($url);
-        return $response->json()['request_token'];
-    }
-
     public function searchMovieOrSerie(array $data, string $movie, int $page)
     {
-        $token = $this->getToken();
         $query = $data['movie'] ?? $movie;
         $url = config('constants.apiUrl') . "/search/multi?api_key=".config('constants.apiKey')."&query=$query&page=$page";
         return Http::get($url)->json();
