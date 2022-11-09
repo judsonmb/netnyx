554f4df3c97a15a8cbc87022b8023d46605dafa0
Judson Bandeira <judson.bandeira@fichacerta.com.br>
2022-11-09 11:55:31 -0300

    melhorias e refatorações

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,32 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-
-                    {{ __('You are logged in!') }}
+                <div class="card-header">Olá, {{ Auth::user()->name }}!</div>
+                    <div class="card-body">
+                        @if (Auth::user()->role == 'admin')
+                            <a href="{{ route('admin') }}">Ver painel administrativo</a>
+                        @elseif (Auth::user()->role == 'customer')
+                            <form action="{{ route('search') }}" method="GET">
+                                @csrf
+                                <div class="input-group">
+                                    <input type="text" 
+                                            class="form-control @error('movie') is-invalid @enderror" 
+                                            id="movie" name="movie" maxlength="255" 
+                                            value="{{ old('movie') }}" 
+                                            placeholder="Digite aqui o filme ou série que deseja buscar" 
+                                            required>
+                                    <button type="submit" class="btn btn-primary">Buscar</button>
+                                    
+                                    @error('movie')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </form>
+                        @else
+                            Você está logado!
+                        @endif
                 </div>
             </div>
         </div>
